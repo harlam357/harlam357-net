8d7cc55f9b14d391d7bc3f15b6e56a9d3dc45267
harlam357 <harlam357@d52a5fc2-0a16-11df-8772-5d79768f539e>
2010-01-27 01:05:45 +0000

    - harlam357-net v1.0.2 Initial Import - .NET 2.0 & .NET 3.5 Configurations Supported

---
+++ trunk/Security/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("harlam357.Security")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("harlam357")]
+[assembly: AssemblyProduct("harlam357-net")]
+[assembly: AssemblyCopyright("Copyright © Ryan Harlamert 2009-2010.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4FAB04E2-C65A-4C1B-95F4-449B1E1E022E")]
+
---
+++ trunk/Security/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("harlam357.Security.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("harlam357")]
+[assembly: AssemblyProduct("harlam357-net")]
+[assembly: AssemblyCopyright("Copyright © Ryan Harlamert 2009-2010.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("FC94C286-0B44-48E7-B406-79ACFDD81045")]
+
