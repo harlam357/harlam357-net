adeb4d6cdd1a316fc2b37dce8de1ebbcaf14e29f
harlam357 <harlam357@d52a5fc2-0a16-11df-8772-5d79768f539e>
2010-05-04 02:23:14 +0000

    - Change: Add code to Verify Downloaded update file based on expected Size, MD5, & SHA1 hashes.

--- Windows.Forms/Tests/UpdatePresenterTests.cs
+++ Windows.Forms/Tests/UpdatePresenterTests.cs
@@ -1,8 +1,27 @@
-﻿
+﻿/*
+ * harlam357.Net - Application Update Presenter Tests
+ * Copyright (C) 2010 Ryan Harlamert (harlam357)
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 using System;
 using System.IO;
 using System.Net;
 using System.Windows.Forms;
+
 using NUnit.Framework;
 using Rhino.Mocks;
 
@@ -81,8 +100,6 @@ namespace harlam357.Windows.Forms.Tests
          mocks.VerifyAll();
       }
 
-      
-
       private static void PerformDownload(string url)
       {
 
@@ -96,6 +113,12 @@ namespace harlam357.Windows.Forms.Tests
       private static void LogException(Exception ex)
       {
 
+      }
+      
+      [Test]
+      public void VerifyDownloadTest()
+      {
+         UpdatePresenter.VerifyDownload("..\\..\\TestFile.txt", _update.UpdateFiles[0]);
       }
    }
 }
