ceab2c3bde7b03b24bf234a8f2f287ae1bd999de
harlam357 <harlam357@d52a5fc2-0a16-11df-8772-5d79768f539e>
2013-11-12 02:08:10 +0000

    - Add Core Project including StreamExtensions and unit tests.

---
+++ Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("harlam357.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ce188740-0ea9-41be-ba52-f4e054c6ccc4")]
---
+++ Core/Tests/StreamExtensionsTests.cs
@@ -0,0 +1,150 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+using harlam357.Core.IO;
+
+namespace harlam357.Core.Tests
+{
+   [TestFixture]
+   public class StreamExtensionsTests
+   {
+      [Test]
+      [ExpectedException(typeof(ArgumentNullException))]
+      public void CopyTo_ArgumentNullException_Test1()
+      {
+         using (var ms = new MemoryStream())
+         {
+            StreamExtensions.CopyTo(null, ms);
+         }
+      }
+
+      [Test]
+      [ExpectedException(typeof(ArgumentNullException))]
+      public void CopyTo_ArgumentNullException_Test2()
+      {
+         using (var ms = new MemoryStream())
+         {
+            StreamExtensions.CopyTo(ms, null);
+         }
+      }
+
+      [Test]
+      [ExpectedException(typeof(ArgumentException))]
+      public void CopyTo_ArgumentException_Test1()
+      {
+         using (var ms1 = new MemoryStream())
+         using (var ms2 = new MemoryStream())
+         {
+            StreamExtensions.CopyTo(ms1, ms2, -1);
+         }
+      }
+
+      [Test]
+      [ExpectedException(typeof(InvalidOperationException))]
+      public void CopyTo_InvalidOperationException_Test1()
+      {
+         MemoryStream ms1;
+         MemoryStream ms2;
+         using (ms1 = new MemoryStream())
+         {
+            
+         }
+         using (ms2 = new MemoryStream())
+         {
+            // input not open for reading
+            StreamExtensions.CopyTo(ms1, ms2);
+         }
+      }
+
+      [Test]
+      [ExpectedException(typeof(InvalidOperationException))]
+      public void CopyTo_InvalidOperationException_Test2()
+      {
+         MemoryStream ms1;
+         MemoryStream ms2;
+         using (ms2 = new MemoryStream())
+         {
+
+         }
+         using (ms1 = new MemoryStream())
+         {
+            // output not open for writing
+            StreamExtensions.CopyTo(ms1, ms2);
+         }
+      }
+
+      [Test]
+      public void CopyTo_Test1()
+      {
+         var buffer = new byte[Int16.MaxValue * 3];
+         var random = new Random();
+         random.NextBytes(buffer);
+         using (var ms1 = new MemoryStream(buffer))
+         using (var ms2 = new MemoryStream())
+         {
+            ms1.CopyTo(ms2);
+            Assert.IsTrue(ms1.ToArray().SequenceEqual(ms2.ToArray()));
+         }
+      }
+
+      [Test]
+      public void FindLastIndex_Test1()
+      {
+         using (var fs = new FileStream(@"..\..\TestFiles\gpl-3.0-1.txt", FileMode.Open, FileAccess.Read))
+         {
+            Assert.AreEqual(1108, fs.FindLastIndex(value => value == Convert.ToInt32('\n')));
+         }
+      }
+
+      [Test]
+      public void Test1()
+      {
+         var lines = new List<string>();
+         long position = 0;
+
+         position = ReadFile(@"..\..\TestFiles\gpl-3.0-1.txt", position, lines);
+         position = ReadFile(@"..\..\TestFiles\gpl-3.0-2.txt", position, lines);
+         position = ReadFile(@"..\..\TestFiles\gpl-3.0-3.txt", position, lines);
+
+         Assert.AreEqual(48, lines.Count);
+      }
+
+      private static long ReadFile(string path, long position, IList<string> lines)
+      {
+         using (var stream = new StreamReader(path))
+         {
+            long newPosition = stream.BaseStream.FindLastIndex(position, Predicate);
+            if (newPosition >= 0)
+            {
+               stream.BaseStream.Position = newPosition + 1;
+               lines.RemoveAt(lines.Count - 1);
+            }
+
+            while (!stream.EndOfStream)
+            {
+               lines.Add(stream.ReadLine());
+            }
+
+            return stream.BaseStream.Position;
+         }
+      }
+
+      private static bool Predicate(int value)
+      {
+         return value == Convert.ToInt32('\r') || value == Convert.ToInt32('\n');
+      }
+
+      //[Test]
+      //public void Test2()
+      //{
+      //   var list = new List<int> {1, 2, 4, 3, 1, 2, 4, 3, 2, 1, 4};
+      //   Assert.AreEqual(10, list.FindLastIndex(x => x == 4));
+      //   Assert.AreEqual(2, list.FindLastIndex(3, x => x == 4));
+      //}
+   }
+}
