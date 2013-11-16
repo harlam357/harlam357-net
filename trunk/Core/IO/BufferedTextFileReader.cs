885f6c1cc206b6f04bde1ef417f388ca991c5cef
harlam357 <harlam357@d52a5fc2-0a16-11df-8772-5d79768f539e>
2013-11-16 04:54:40 +0000

    - Add BufferedTextFileReader and tests.

---
+++ trunk/Core/IO/BufferedTextFileReader.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace harlam357.Core.IO
+{
+   public sealed class BufferedTextFileReader
+   {
+      private readonly List<string> _items;
+
+      public BufferedTextFileReader()
+      {
+         _items = new List<string>();
+      }
+
+      public IEnumerable<string> ReadAllLines(string path, ref StreamPosition position)
+      {
+         return ReadAllLines(path, Encoding.UTF8, ref position);
+      }
+
+      public IEnumerable<string> ReadAllLines(string path, Encoding encoding, ref StreamPosition position)
+      {
+         using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
+         using (var sr = new StreamReader(fs, encoding))
+         {
+            if (!fs.SetStreamPosition(position))
+            {
+               _items.Clear();
+            }
+
+            while (!sr.EndOfStream)
+            {
+               _items.Add(sr.ReadLine());
+            }
+
+            ResetEndOfStream(_items, fs);
+            position = fs.GetStreamPosition(128);
+         }
+
+         return _items;
+      }
+
+      private static void ResetEndOfStream(IList<string> lines, Stream fs)
+      {
+         long streamPosition = fs.Position;
+         long lastCrLfPosition = fs.FindLastIndex(Predicate);
+         if (streamPosition != lastCrLfPosition)
+         {
+            lines.RemoveAt(lines.Count - 1);
+            fs.Position = lastCrLfPosition;
+         }
+      }
+
+      private static bool Predicate(int value)
+      {
+         return value == Convert.ToInt32('\r') || value == Convert.ToInt32('\n');
+      }
+   }
+}
---
+++ trunk/Core/Tests/IO/BufferedTextFileReaderTests.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace harlam357.Core.IO
+{
+   [TestFixture]
+   public class BufferedTextFileReaderTests
+   {
+      [Test]
+      public void BufferedTextFileReader_ReadAllLines_Test1()
+      {
+         var reader = new BufferedTextFileReader();
+         StreamPosition position = StreamPosition.Empty;
+         List<string> lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-1.txt", ref position).ToList();
+         Assert.AreEqual(23, lines.Count);
+         lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-2.txt", ref position).ToList();
+         Assert.AreEqual(39, lines.Count);
+         lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-3.txt", ref position).ToList();
+         Assert.AreEqual(48, lines.Count);
+         var compareLines = File.ReadAllLines(@"..\..\TestFiles\gpl-3.0-3.txt");
+         Assert.IsTrue(lines.SequenceEqual(compareLines));
+      }
+   }
+}
