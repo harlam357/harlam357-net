69b02c85e5591bed8c5ef76073ee96ac1340b15e
harlam357 <harlam357@d52a5fc2-0a16-11df-8772-5d79768f539e>
2013-11-28 15:13:43 +0000

    - Update BufferedTextFileReader implementation and tests.

--- Core/IO/BufferedTextFileReader.cs
+++ Core/IO/BufferedTextFileReader.cs
@@ -8,24 +8,45 @@ namespace harlam357.Core.IO
 {
    public sealed class BufferedTextFileReader
    {
+      private StreamPosition _position;
       private readonly List<string> _items;
 
       public BufferedTextFileReader()
       {
+         _position = StreamPosition.Empty;
          _items = new List<string>();
       }
 
-      public IEnumerable<string> ReadAllLines(string path, ref StreamPosition position)
+      public IEnumerable<string> ReadAllLines(string path)
       {
-         return ReadAllLines(path, Encoding.UTF8, ref position);
+         return ReadAllLines(path, Encoding.UTF8);
       }
 
-      public IEnumerable<string> ReadAllLines(string path, Encoding encoding, ref StreamPosition position)
+      public IEnumerable<string> ReadAllLines(string path, Encoding encoding)
       {
          using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
-         using (var sr = new StreamReader(fs, encoding))
          {
-            if (!fs.SetStreamPosition(position))
+            return ReadAllLines(fs, encoding);
+         }
+      }
+
+      public IEnumerable<string> ReadAllLines(Stream stream)
+      {
+         return ReadAllLines(stream, Encoding.UTF8);
+      }
+
+      public IEnumerable<string> ReadAllLines(Stream stream, Encoding encoding)
+      {
+         using (var sr = new StreamReader(stream, encoding))
+         {
+            if (stream.SetStreamPosition(_position))
+            {
+               if (!IsEndOfLine(_position.CheckBuffer))
+               {
+                  ResetStream(_items, stream);
+               }
+            }
+            else
             {
                _items.Clear();
             }
@@ -35,27 +56,32 @@ namespace harlam357.Core.IO
                _items.Add(sr.ReadLine());
             }
 
-            ResetEndOfStream(_items, fs);
-            position = fs.GetStreamPosition(128);
+            _position = stream.GetStreamPosition(128);
          }
 
          return _items;
       }
 
-      private static void ResetEndOfStream(IList<string> lines, Stream fs)
+      private static bool IsEndOfLine(byte[] buffer)
       {
-         long streamPosition = fs.Position;
-         long lastCrLfPosition = fs.FindLastIndex(Predicate);
-         if (streamPosition != lastCrLfPosition)
+         if (buffer.Length < 2)
          {
-            lines.RemoveAt(lines.Count - 1);
-            fs.Position = lastCrLfPosition;
+            return false;
          }
+         return (buffer[buffer.Length - 2] == Convert.ToByte('\r') &&
+                 buffer[buffer.Length - 1] == Convert.ToByte('\n')) ||
+                 buffer[buffer.Length - 2] == Convert.ToByte('\n');
       }
 
-      private static bool Predicate(int value)
+      private static void ResetStream(IList<string> lines, Stream stream)
       {
-         return value == Convert.ToInt32('\r') || value == Convert.ToInt32('\n');
+         long streamPosition = stream.Position;
+         long crLfPosition = stream.FindLastIndex(streamPosition, value => value == Convert.ToInt32('\n'));
+         if (crLfPosition >= 0 && streamPosition != crLfPosition)
+         {
+            lines.RemoveAt(lines.Count - 1);
+            stream.Position = crLfPosition;
+         }
       }
    }
 }
--- Core/Tests/IO/BufferedTextFileReaderTests.cs
+++ Core/Tests/IO/BufferedTextFileReaderTests.cs
@@ -1,7 +1,9 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 using NUnit.Framework;
 
@@ -14,15 +16,81 @@ namespace harlam357.Core.IO
       public void BufferedTextFileReader_ReadAllLines_Test1()
       {
          var reader = new BufferedTextFileReader();
-         StreamPosition position = StreamPosition.Empty;
-         List<string> lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-1.txt", ref position).ToList();
-         Assert.AreEqual(23, lines.Count);
-         lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-2.txt", ref position).ToList();
-         Assert.AreEqual(39, lines.Count);
-         lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-3.txt", ref position).ToList();
+         List<string> lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-1.txt").ToList();
+         Assert.AreEqual(24, lines.Count);
+         lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-2.txt").ToList();
+         Assert.AreEqual(40, lines.Count);
+         lines = reader.ReadAllLines(@"..\..\TestFiles\gpl-3.0-3.txt").ToList();
          Assert.AreEqual(48, lines.Count);
          var compareLines = File.ReadAllLines(@"..\..\TestFiles\gpl-3.0-3.txt");
          Assert.IsTrue(lines.SequenceEqual(compareLines));
+      }
+
+      [Test]
+      public void BufferedTextFileReader_ReadAllLines_Test2()
+      {
+         ReadAllLines_Internal_Test("First line.\r\n", "Second line.", "\r\nThird line.\r\n", "Fourth line.\r\n");
+      }
+
+      [Test]
+      public void BufferedTextFileReader_ReadAllLines_Test3()
+      {
+         ReadAllLines_Internal_Test("First line.\n", "Second line.", "\nThird line.\n", "Fourth line.\n");
+      }
+
+      [Test]
+      public void BufferedTextFileReader_ReadAllLines_Test4()
+      {
+         ReadAllLines_Internal_Test("First lin", "e.\r\nSecond line.\r\n", "Third", " line.\r\nFourth line.");
+      }
+
+      [Test]
+      public void BufferedTextFileReader_ReadAllLines_Test5()
+      {
+         ReadAllLines_Internal_Test("First lin", "e.\nSecond line.\n", "Third", " line.\nFourth line.");
+      }
+
+      public void ReadAllLines_Internal_Test(params string[] input)
+      {
+         List<string> lines;
+         var reader = new BufferedTextFileReader();
+
+         byte[] buffer1 = Encoding.UTF8.GetBytes(input[0]);
+         byte[] buffer2 = Encoding.UTF8.GetBytes(input[1]);
+         byte[] buffer3 = Encoding.UTF8.GetBytes(input[2]);
+         byte[] buffer4 = Encoding.UTF8.GetBytes(input[3]);
+
+         using (var ms = new MemoryStream())
+         {
+            ms.Write(buffer1, 0, buffer1.Length);
+            ms.Write(buffer2, 0, buffer2.Length);
+
+            lines = reader.ReadAllLines(ms).ToList();
+            Assert.AreEqual(2, lines.Count);
+         }
+
+         using (var ms = new MemoryStream())
+         {
+            ms.Write(buffer1, 0, buffer1.Length);
+            ms.Write(buffer2, 0, buffer2.Length);
+            ms.Write(buffer3, 0, buffer3.Length);
+            ms.Write(buffer4, 0, buffer4.Length);
+
+            lines = reader.ReadAllLines(ms).ToList();
+            Assert.AreEqual(4, lines.Count);
+         }
+
+         Assert.IsTrue(lines.SequenceEqual(CreateCompareLines()));
+      }
+
+      private static IEnumerable<string> CreateCompareLines()
+      {
+         var sb = new StringBuilder();
+         sb.AppendLine("First line.");
+         sb.AppendLine("Second line.");
+         sb.AppendLine("Third line.");
+         sb.Append("Fourth line.");
+         return sb.ToString().Split(new[] { Environment.NewLine }, StringSplitOptions.None);
       }
    }
 }
