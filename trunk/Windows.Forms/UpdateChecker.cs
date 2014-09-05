996b67482672e4da24bfefa80800fa671a5da618
harlam357 <harlam357@d52a5fc2-0a16-11df-8772-5d79768f539e>
2014-09-05 22:56:30 +0000

    - Add RequestCachePolicy(RequestCacheLevel.NoCacheNoStore) to ApplicationUpdateXml download.

--- trunk/Windows.Forms/UpdateChecker.cs
+++ trunk/Windows.Forms/UpdateChecker.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * harlam357.Net - Update Checker Class
- * Copyright (C) 2010-2013 Ryan Harlamert (harlam357)
+ * Copyright (C) 2010-2014 Ryan Harlamert (harlam357)
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,6 +20,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Net.Cache;
 
 using harlam357.Core.Net;
 
@@ -50,6 +51,7 @@ namespace harlam357.Windows.Forms
          
          IWebOperation web = WebOperation.Create(updateUrl);
          if (proxy != null) web.WebRequest.Proxy = proxy;
+         web.WebRequest.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
          web.Download(localFilePath);
          return ApplicationUpdateSerializer.DeserializeFromXml(localFilePath);
       }
